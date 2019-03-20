473b4653f561ca8a1539ac16a56690b9bccc3550
nhyox <32385457+nhyox@users.noreply.github.com>
2019-03-20 12:07:22 +0100

    The new Map

---
+++ Core/src/main/resources/assets/images/Asets.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Asets" tilewidth="32" tileheight="32" tilecount="112" columns="14">
+ <image source="../Pictures/TIlesets/Asets.png" width="448" height="256"/>
+ <tile id="31">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+</tileset>
