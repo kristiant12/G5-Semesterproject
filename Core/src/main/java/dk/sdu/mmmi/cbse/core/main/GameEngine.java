de586babaa04070e00c20afc9d14972efae6a2eb
Kaspermn33 <32263696+Kaspermn33@users.noreply.github.com>
2019-03-28 12:26:29 +0100

    Removed souts

--- Core/src/main/java/dk/sdu/mmmi/cbse/core/main/GameEngine.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/core/main/GameEngine.java
@@ -87,9 +87,6 @@ public class GameEngine implements ApplicationListener {
 
         gameData.getKeys().update();
         //renderer.setView(cam);
-        System.out.println("Y = " + (Gdx.input.getY() - Gdx.graphics.getHeight()/2));
-        System.out.println("X = " + (Gdx.input.getX() - Gdx.graphics.getWidth()/2));
-        System.out.println("");
         gameData.setMouseX(Gdx.input.getX() - Gdx.graphics.getWidth()/2);
         gameData.setMouseY(Gdx.graphics.getHeight()/2 - Gdx.input.getY());
     }
