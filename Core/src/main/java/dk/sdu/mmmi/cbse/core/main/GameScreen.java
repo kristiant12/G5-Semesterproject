9b92fd80c4b81ef5381706fdaefc10876c4e718f
kristiant12 <32262503+kristiant12@users.noreply.github.com>
2019-04-11 12:22:43 +0200

    Nicolai er nazist

--- Core/src/main/java/dk/sdu/mmmi/cbse/core/main/GameScreen.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/core/main/GameScreen.java
@@ -8,33 +8,24 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
  * @author Monica
  */
 public class GameScreen implements Screen {
-    //Creates a reference to hold our "passed" game. Change "AdlermacGame" to you game class name.
-    private AdlermacGame game;
 
-    //Defining the camera to be used
     private OrthographicCamera camera;
+    private GameEngine game;
 
-    public GameScreen(AdlermacGame game){
-        //assigns the game we passed to the placeholder
+    public GameScreen(GameEngine game){
         this.game=game;
-        //creates the camera
         camera = new OrthographicCamera();
-        //Sets the screen to be 800 width by 480 height
         camera.setToOrtho(false,800,480);
     }
 
-    //this is called every time the game is rendered
     @Override
     public void render(float deltaTime){
-        //Sets the color to be applied after clearing the screen (R,G,B,A)
         Gdx.gl.glClearColor(0,0,255,1);
-        //Clears the screen
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
 
-        //updates camera view
         camera.update();
 
-        //makes the camera fit onto the available screen
         game.gameBatch.setProjectionMatrix(camera.combined);
 
     }
